0b3de7f00365336e3069edbf00c947addce2ef9d
Fred Yang <fredyang@semanticsworks.com>
2023-12-30 14:09:25 -0500

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Your Receipt';
 
-  constructor(private titleService: Title) {
-    this.titleService.setTitle($localize`${this.title}`);
-  }
+  now = new Date();
 
+  constructor(private titleService: Title, @Inject(LOCALE_ID) locale: string) {
+    console.log(locale);
+    this.titleService.setTitle(
+      $localize`Your Receipt ${this.title}:my_place_holder:`
+    );
+  }
 }

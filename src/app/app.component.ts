77a6814fa16b87cb8579782089bfe43e5933c027
Fred Yang <fredyang@semanticsworks.com>
2023-12-30 16:55:59 -0500

    add example of  icu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,20 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Your Receipt';
+  title = 'Your Receipt one';
 
   now = new Date();
 
   constructor(private titleService: Title, @Inject(LOCALE_ID) locale: string) {
     console.log(locale);
-    this.titleService.setTitle(
-      $localize`Your Receipt ${this.title}:my_place_holder:`
-    );
+    // this.titleService.setTitle(
+    //   $localize`Your Receipt ${this.title}:my_place_holder:`
+    // );
+
+    this.titleService.setTitle($localize`${this.title}`);
+    console.log(this.title);
   }
+
+  gender = 'other';
+  minutes = 1;
 }
